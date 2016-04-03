d5f08fde8cf9c43675f12fd86ce53987545adf4a
mattd <mattd@mattd.(none)>
2016-04-03 22:37:12 +0200

    Afegit admin per Artist/Album/Track

--- MusicProject/MusicApp/admin.py
+++ MusicProject/MusicApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from MusicApp.models import Album, Artist, Track
+
+admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Track)
