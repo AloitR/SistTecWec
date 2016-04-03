283062a12c52d0277f76f3658cbf33fbec0c72b2
mattd <mattd@mattd.(none)>
2016-04-03 22:41:25 +0200

    Afegifes claus foranes

--- MusicProject/MusicApp/models.py
+++ MusicProject/MusicApp/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
 
 class Artist(models.Model):
     nomArtista = models.TextField(max_length=30)
+    album = models.ForeignKey(Album)
+    track = models.ForeignKey(Track)
 
 class Album(models.Model):
     nomAlbum = models.TextField(max_length=30)
+    artist = models.ForeignKey(Artist)
+    track = models.ForeignKey(Track)
 
 class Track(models.Model):
     nomTrack = models.TextField(max_length=30)
+    artist = models.ForeignKey(Artist)
+    album = models.ForeignKey(Album)
