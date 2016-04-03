c6bdb1e6c522efa1136a661ccf48192790a26282
mattd <mattd@mattd.(none)>
2016-04-03 22:33:27 +0200

    Model per Artist/Album/Track (Dades que es demanarán)

--- MusicProject/MusicApp/models.py
+++ MusicProject/MusicApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    nomArtista = models.TextField(max_length=30)
+
+class Album(models.Model):
+    nomAlbum = models.TextField(max_length=30)
+
+class Track(models.Model):
+    nomTrack = models.TextField(max_length=30)
