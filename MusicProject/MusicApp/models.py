6cfe6706174c82eb2ab542b091a4bf1e591633da
mattd <mattd@mattd.(none)>
2016-04-04 20:11:41 +0200

    Afegida pagina d'usuari. /user/<username>

--- MusicProject/MusicApp/models.py
+++ MusicProject/MusicApp/models.py
@@ -5,9 +5,22 @@ from django.contrib.auth.models import User
 
 class Artist(models.Model):
     nomArtista = models.TextField()
+    tags = models.TextField()
+    url = models.TextField()
+    similars = models.TextField()
+    summary = models.TextField()
 
 class Album(models.Model):
     nomAlbum = models.TextField()
+    tag = models.TextField()
+    releasedate = models.TextField()
+    url = models.TextField()
+
 
 class Track(models.Model):
     nomTrack = models.TextField()
+    url = models.TextField()
+    duration = models.IntegerField()
+    playcount = models.IntegerField()
+    published = models.TextField()
+    summary = models.TextField()
